e47fcbdede973dbc0bf23176ca4dfbc6f5b0ef5b
aruiz-duxstar <aruiz@duxstar.com>
2024-04-21 19:47:52 -0600

    Se actualiza la primer parte de la pagina con una imagen de Google

--- index.php
+++ index.php
@@ -3,21 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Empresa</title>
+    <title>Nuestra Empresa</title>
     <link rel="stylesheet" href="styles.css">
+
 </head>
 <body class="content">
 
-    <div class="linea-horizontal"> <!-- informacion-->
-        <div>Corre@gmail.com</div>
-        <div>Telefono </div>
-        <div>Soporte</div>
-        <div>Ayuda</div>
+    <div>
+        <div class="linea-horizontal">
+            <div class="email-linea">Corre@gmail.com</div>
+            <div class="phone-linea">Telefono</div>
+            <div class="support-linea">Soporte</div>
+            <div class="help-linea">Ayuda</div>
+        </div>
     </div>
 
-    <div> <!-- Contenedor --> 
-        <div></div>
+    <div class="content-home">
+        <div><a><img src="img/google.png" alt="" width="80" height="50"><div class="hoster-menu"> Empresa</div></a></div>
+        <div><a class="home-menu">Home</a></div>
+        <div><a class="dominio-menu">Dominio</a></div>
+        <div><a class="wh-menu">Web Hosting</a></div>
+        <div><a class="server-menu">Servidores</a></div>
+        <div><a class="seg-menu">Seguridad</a></div>
+        <div><button type="button" onclick="window.location.href='#'" class="boton-login">Login</button></div>
     </div>
-</div>
+
+
+    <div class="home-txt">
+        <p><h1>Reliable Web Hosting Platform for Your Website</h1></p>
+        <p>Fully Managed High Performance Web Hosting With Free Domain</p>
+        <div><button class="free-button">7 dias</button></div>
+        <div><button class="plan-button">Cambia tu plan</button></div>
+    </div>
+    <div >
+        <div>
+        <img src="img/google.png"class="home-img"></img>
+        </div>
+    </div>
+
+
+    <div>
+        <div class="dom-home"></div>
+    </div>
 </body>
 </html>
