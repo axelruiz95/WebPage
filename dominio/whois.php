062cbfa2742cbac9be3edd012622a8f66a0c5c62
aruiz-duxstar <aruiz@duxstar.com>
2024-08-04 21:22:09 -0600

    Pruebas Whois

--- dominio/whois.php
+++ dominio/whois.php
@@ -1,42 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
 
-    <!DOCTYPE html>
-    <html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta WHOIS</title>
+    <link rel="stylesheet" href="../styles/styles.css">
+</head>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Consulta WHOIS</title>
-        <link rel="stylesheet" href="../styles/styles.css">
-    </head>
-
-    <body class="content">
-        <?php
-        include '../menu/header.php';
-        ?>
-
-        <h1>Consulta WHOIS de Dominio</h1>
-        <form action="" method="post">
-            <label for="domain">Ingresa el dominio:</label>
-            <input type="text" id="domain" name="domain" required>
-            <button type="submit">Consultar WHOIS</button>
-        </form>
-
-        <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['domain'])) {
-    $domain = htmlspecialchars($_POST['domain']);
-
-    $escaped_domain = escapeshellarg($domain);
-
-    $whois = shell_exec("whois $escaped_domain");
-
-    if ($whois === null) {
-        echo "<p>No se pudo ejecutars WHOIS..</p>";
-    } else {
-        echo "<h1>Resultados de WHOIS</h1>";
-        echo "<pre>$whois</pre>";
-    }
-} else {
+<body class="content">
+    <?php
+    include '../menu/header.php';
     ?>
+
+    <h1>Consulta WHOIS de Dominio</h1>
+    <form action="" method="post">
+        <label for="domain">Ingresa el dominio:</label>
+        <input type="text" id="domain" name="domain" required>
+        <button type="submit">Consultar WHOIS</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['domain'])) {
+        $domain = htmlspecialchars($_POST['domain']);
+
+        $escaped_domain = escapeshellarg($domain);
+
+        $whois = shell_exec("whois $escaped_domain");
+
+        if ($whois === null) {
+            echo "<p>No se pudo ejecutars WHOIS..</p>";
+        } else {
+            echo "<h1>Resultados de WHOIS</h1>";
+            echo "<pre>$whois</pre>";
+        }
+    } else {}
+        ?>
         <div></div>
         <?php
         include '../menu/footer.php';
@@ -46,6 +45,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['domain'])) {
     </body>
 
     </html>
-    <?php
-}
-?>
